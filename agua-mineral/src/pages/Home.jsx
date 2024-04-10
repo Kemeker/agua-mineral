e237f10085c60e66b1f83eb919af7af7928812a4
Kemeker <kemeker@gmail.com>
2024-04-10 20:56:03 -0300

    Feat: realiado primeiro nav com Tailwind

--- agua-mineral/src/pages/Home.jsx
+++ agua-mineral/src/pages/Home.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import '../css/App.css'
 import NavBar from '../components/NavBar'
 
+
 function Home(){
     return(
-
+        
         <NavBar />
+
+
+
+       
+
+
     )
 }
 export default Home
